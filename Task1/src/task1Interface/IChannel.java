7ffb94595b47fce9e81d5befedfb17b3fad78736
Hannane Mahamoudou <hannane.mahamoudou@etu.univ-grenoble-alpes.fr>
2024-09-24 22:16:01 +0200

    Update IChannel

--- Task1/src/task1Interface/IChannel.java
+++ Task1/src/task1Interface/IChannel.java
@@ -1,8 +1,10 @@
 package task1Interface;
 
+import exception.DisconnectedException;
+
 public interface IChannel {
-int read(byte[] bytes, int offset, int length);
-    int write(byte[] bytes, int offset, int length);
+int read(byte[] bytes, int offset, int length) throws DisconnectedException;
+    int write(byte[] bytes, int offset, int length) throws DisconnectedException;
     void disconnect();
     boolean disconnected();
 }
