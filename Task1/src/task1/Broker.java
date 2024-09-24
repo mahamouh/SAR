89526ed765cbc41e7969cc8baa701f855661d119
Hannane Mahamoudou <hannane.mahamoudou@etu.univ-grenoble-alpes.fr>
2024-09-24 22:13:42 +0200

    Ajout des implements des classes

--- Task1/src/task1/Broker.java
+++ Task1/src/task1/Broker.java
@@ -1,22 +1,68 @@
 package task1;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import task1Interface.*;
 
-public class Broker implements IBroker{
-	public Broker(String name) {
-		
+public class Broker implements IBroker {
+	String name;
+	private HashMap<Integer, RdV> rdV;
+	BrokerManagement brokerManagement;
+
+	public Broker(String name, BrokerManagement brokerManagement) {
+		this.name = name;
+		this.rdV = new HashMap<Integer, RdV>();
+		this.brokerManagement = brokerManagement;
+	}
+
+	public void setBroker(BrokerManagement brokerManagement) {
+		this.brokerManagement = brokerManagement;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public void addRdV(int port) {
+		RdV rdv = new RdV(port);
+		this.rdV.put(port, rdv);
+	}
+
+	public void removeRdV(int port) {
+		this.rdV.remove(port);
+	}
+
+	public RdV containsRdV(int port) {
+		RdV rdv = this.rdV.get(port);
+		return rdv;
 	}
 
 	@Override
 	public Channel accept(int port) {
-		// TODO Auto-generated method stub
-		return null;
+		synchronized (this) {
+			RdV rdv = containsRdV(port);
+			if (rdv == null) {
+				rdv = new RdV(port);
+				this.rdV.put(port, rdv);
+			} 
+			return rdv.accept(this);
+		}
 	}
 
 	@Override
 	public Channel connect(String name, int port) {
-		// TODO Auto-generated method stub
-		return null;
+		synchronized(this) {
+			RdV rdv = containsRdV(port);
+			if (rdv == null) {
+				rdv = new RdV(port);
+				this.rdV.put(port, rdv);
+				if(brokerManagement.getBroker(name) !=null) {
+					brokerManagement.getBroker(name).addRdV(port);
+				}
+			}
+			return rdv.connect(this);
+		}
 	}
 
 }
--- Task1/src/task1/Task.java
+++ Task1/src/task1/Task.java
@@ -3,14 +3,24 @@ package task1;
 import task1Interface.ITask;
 
 public class Task extends Thread implements ITask{
+	Broker broker;
+	Runnable runnable;
+	
 	public Task(Broker b, Runnable r) {
-		
+		this.broker = b;
+		this.runnable = r;
+	}
+	
+	@Override
+	public void run() {
+		if(this.runnable != null) {
+			this.runnable.run();
+		}
 	}
 
 	@Override
 	public Broker getBroker() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.broker;
 	}
 
 }
